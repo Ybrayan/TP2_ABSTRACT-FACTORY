03b97c0c772548a79fa2b129b33b6177b559c33c
yhors <ybrayan13@gmail.com>
2020-04-15 19:16:03 -0500

    first commit

---
+++ car.js
@@ -0,0 +1,13 @@
+class Car{
+    constructor(
+        engine_number,
+        chassis_number,
+        fuel,
+        brand,
+        model,
+        color,
+        country
+    ){
+        
+    }
+}
